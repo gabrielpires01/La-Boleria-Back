ea4530362587df0cba7fffe5b57ec77fcf9c7ad0
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-03-18 17:32:47 -0300

    Fixed Status errors codes for Validation

--- src/middlewares/validateSchemaMiddlesware.js
+++ src/middlewares/validateSchemaMiddlesware.js
@@ -1,6 +1,10 @@
 const validateSchemaMiddleware = schema => {
 	return(req,res,next) => {
 		const validate = schema.validate(req.body);
+		
+		if (validate.error?.details[0].type === "string.empty") return res.sendStatus(400)
+		if (validate.error?.details[0].type === "any.required") return res.sendStatus(400)
+		if (validate.error?.details[0].type === "string.base") return res.sendStatus(400)
 		if (validate.error) return res.sendStatus(422)
 
 		next()
