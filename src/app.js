fb355e4aa197776e06194a882fdeba4c270b4fe4
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-03-18 01:27:33 -0300

    Inicialize Project

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import express, { json } from 'express';
+import router from './routes.js';
+dotenv.config()
+
+const app = express();
+app.use(json());
+
+app.use(router)
+
+const PORT = 5000;
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`)
+})
