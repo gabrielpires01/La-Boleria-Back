5140211454e98f52d872e0224c6c82b9dab004bc
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-03-18 04:02:24 -0300

    Get Client Orders route created

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -15,4 +15,31 @@ const postClients = async(req, res) => {
 	}
 }
 
-export { postClients };
+const getClientOrder = async(req, res) => {
+	const { id } = req.params;
+
+	try {
+		const client = await connection.query(`SELECT * FROM clients WHERE id = $1`, [id]);
+		if (!client.rowCount) return res.sendStatus(404)
+
+		const orders = await connection.query(`SELECT * FROM orders WHERE "clientId" = $1`, [id]);
+
+		const clientOrders = await Promise.all(orders.rows.map(async(order)  => {
+			const cake = await connection.query(`SELECT * FROM cakes WHERE id = $1`, [order.cakeId]);
+
+			return {
+				orderId: order.id,
+				quantity: order.quantity,
+				cretedAt: order.createdAt,
+				totalPrice: order.totalPrice,
+				cakeName: cake.rows[0].name
+			}
+		}))
+
+		return res.status(200).send(clientOrders)
+	}catch(err) {
+		return res.status(500).send(err)
+	}
+}
+
+export { postClients, getClientOrder };
--- src/routes/clientsRouter.js
+++ src/routes/clientsRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { postClients } from "../controllers/clientsController.js";
+import { getClientOrder, postClients } from "../controllers/clientsController.js";
 import validateSchemaMiddleware from "../middlewares/validateSchemaMiddlesware.js";
 import clientValidationSchema from "../schemas/clientValidationSchema.js";
 
 const clientsRouter = Router();
 
 clientsRouter.post('/clients', validateSchemaMiddleware(clientValidationSchema), postClients);
+clientsRouter.get('/clients/:id/orders', getClientOrder);
 
 export default clientsRouter;
